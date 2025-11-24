3db28b95436364e6ee16ff16eb2cc7e6d3b72ea6
shraddha21-prn <shraddha.repale24@sanjivani.edu.in>
2025-11-24 08:20:05 +0530

    Remove vercel.json for Render deployment

--- app.py
+++ app.py
@@ -1174,8 +1174,8 @@ def profile():
 # --------------------------
 # --- Run App -------------
 # --------------------------
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
 
 
         
